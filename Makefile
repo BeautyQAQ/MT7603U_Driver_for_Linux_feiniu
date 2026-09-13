812548bac0daffde5ec6ad8abcba01d7fbc6442b
huang <2316849407@qq.com>
2026-09-13 15:15:40 +0800

    Fix Linux 6.13+ monitor callback and repeatable dark mode builds

--- Makefile
+++ Makefile
@@ -246,7 +246,7 @@ else
 #build mt7603usta.ko
 ifeq ($(DARK_MODE),NO)
 	echo "Dark mode disabled"
-	sed -i '1s/#define/\/\/#define/g'  common/rtusb_dev_id.c
+	sed -i '1c\//#define Fantasy DDFantasies' common/rtusb_dev_id.c
 endif
 	MODULE_FLAGS="$(WFLAGS)"
 	$(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) -C $(LINUX_SRC) M=$(RT28xx_DIR)/os/linux modules
--- os/linux/cfg80211/cfg80211.c
+++ os/linux/cfg80211/cfg80211.c
@@ -400,7 +400,7 @@ static int CFG80211_OpsChannelSet(
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0))
 static int CFG80211_OpsMonitorChannelSet(struct wiphy *pWiphy,
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)) || (LINUX_VERSION_CODE >= KERNEL_VERSION(6,13,0))
 					 struct net_device *dev,
 #endif
 					 struct cfg80211_chan_def *chandef)
